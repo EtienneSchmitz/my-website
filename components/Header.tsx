0615479e08c7894b347a77798ef0b02b16a6dff3
Etienne Schmitz <hello@etienne-schmitz.com>
2020-12-20 11:28:10 +0100

    feat - add react icons, begin header (wip)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,39 @@
 import React from "react";
-
+import { GiSkills } from "react-icons/gi";
+import { FaUniversity } from "react-icons/fa";
+import { AiTwotoneExperiment, AiOutlineInfoCircle } from "react-icons/ai";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <div>
-      {/** Person Icone */}
-      Etienne Schmitz
-      <Link href="/">
-        <a>Mes informations</a>
-      </Link>
-      <Link href="/skills">
-        <a>Mes compétences</a>
-      </Link>
-      <Link href="/study">
-        <a>Mes études</a>
-      </Link>
-      <Link href="/experiences">
-        <a>Mes expériences</a>
-      </Link>
-    </div>
+    <nav className="grid grid-cols-12 grid-rows-1	 p-6 bg-blue-500">
+      <div className="col-start-1 col-end-5 text-white">Etienne Schmitz</div>
+      <div className="col-start-5 col-end-12 flex justify-between">
+        <Link href="/">
+          <a className="flex items-center text-teal-lighter hover:text-white">
+            <AiOutlineInfoCircle />
+            <span className="ml-2">Mes informations</span>
+          </a>
+        </Link>
+        <Link href="/skills">
+          <a className="flex items-center text-teal-lighter hover:text-white">
+            <GiSkills />
+            <span className="ml-1">Mes compétences</span>
+          </a>
+        </Link>
+        <Link href="/study">
+          <a className="flex items-center text-teal-lighter hover:text-white">
+            <FaUniversity />
+            <span className="ml-1">Mes études</span>
+          </a>
+        </Link>
+        <Link href="/experiences">
+          <a className="flex items-center text-teal-lighter hover:text-white">
+            <AiTwotoneExperiment />
+            <span className="ml-1">Mes expériences</span>
+          </a>
+        </Link>
+      </div>
+    </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 // import styles from '../styles/Home.module.css'
-import { useRouter } from "next/router"
-import { useIntl } from "react-intl"
+import { useRouter } from "next/router";
+import { useIntl } from "react-intl";
 
 export default function Home() {
-  const { formatMessage } = useIntl()
-  const f = id => formatMessage({ id })
-  const router = useRouter()
-  const { locale, locales, defaultLocale } = router
-  
+  const { formatMessage } = useIntl();
+  const f = (id) => formatMessage({ id });
+  const router = useRouter();
+  const { locale, locales, defaultLocale } = router;
+
   return (
     <div>
       <h1>{f("hello")}</h1>
@@ -17,6 +17,12 @@ export default function Home() {
       <p>Current locale: {locale}</p>
       <p>Default locale: {defaultLocale}</p>
       <p>Configured locales: {JSON.stringify(locales)}</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta,
+        officia inventore blanditiis sint iure voluptatum perferendis magnam
+        modi aliquam! Possimus, delectus iste. Iste cum minus sequi voluptate
+        optio officia quod.
+      </p>
     </div>
-  )
+  );
 }
